86ba65af68e7b5a39a9245a3fe1f079f5035645d
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-12-28 15:39:34 +0700

    #7 Serving static files with Koa

--- server.js
+++ server.js
@@ -3,8 +3,11 @@
 const Koa = require("koa");
 const logger = require("koa-logger");
 const Router = require("koa-router");
-const db = require("./db");
+const serve = require("koa-static");
+const path = require("path");
+const fs = require("fs");
 
+const db = require("./db");
 const app = new Koa();
 const router = new Router();
 const server = require("http").createServer(app.callback());
@@ -13,8 +16,13 @@ app.use(logger());
 
 app.use(router.routes());
 
+app.use(serve(__dirname + "/frontend/build/"));
+
 router.get("/", async (ctx, next) => {
-  ctx.body = "hello";
+  ctx.type = "html";
+  ctx.body = fs.createReadStream(
+    path.join(__dirname, "/frontend/build/index.html")
+  );
 });
 
 server.listen(8000);
