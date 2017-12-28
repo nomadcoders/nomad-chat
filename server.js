b4cf16823d92bb286bb8b3347503aa291a6c1ddb
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-12-28 14:53:01 +0700

    #3 Creating the server

---
+++ server.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const Koa = require("koa");
+const logger = require("koa-logger");
+
+const app = new Koa();
+const server = require("http").createServer(app.callback());
+
+app.use(logger());
+
+server.listen(8000);
+console.log("Server running on the port 8000 ✅");
