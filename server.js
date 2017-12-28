9fc8eab545cf8d1ff0865ff0998fb090245ee613
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-12-28 15:21:44 +0700

    #5 Creating the Router

--- server.js
+++ server.js
@@ -2,12 +2,20 @@
 
 const Koa = require("koa");
 const logger = require("koa-logger");
+const Router = require("koa-router");
 const db = require("./db");
 
 const app = new Koa();
+const router = new Router();
 const server = require("http").createServer(app.callback());
 
 app.use(logger());
+
+app.use(router.routes());
+
+router.get("/", async (ctx, next) => {
+  ctx.body = "hello";
+});
 
 server.listen(8000);
 console.log("Server running on the port 8000 ✅");
